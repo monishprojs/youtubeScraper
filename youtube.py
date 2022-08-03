b3c93efec1914de43a2adfe14546c64b3321fecb
Monish <monishkumar833@gmail.com>
2022-08-02 23:46:37 -0400

    clicks on video, working on getting metadata

--- youtube.py
+++ youtube.py
@@ -11,25 +11,13 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://youtube.com")
 driver.maximize_window()
-print(driver.title)
-try:
-    search = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "search_query"))
-    )
-except:
-    driver.quit()
-search = driver.find_element(By.NAME, "search_query")
-search.click()
-search.send_keys("coding")
-search.send_keys(Keys.RETURN)
 try:
     title = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, 'video-title'))
     )
 except:
-    driver, quit()
+    driver.quit()
 title = driver.find_element(By.ID, 'video-title')
-metadata = driver.find_elements(By.CLASS_NAME, "ytd-video-meta-block")
-print(title.text)
-for i in range(3):
-    print(metadata[i].text)
+title.click()
+meta = driver.find_elements(By.CLASS_NAME, "yt-formatted-string")
+print(meta[0])
